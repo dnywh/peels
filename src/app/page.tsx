eead9f7d1167ce459b5b06e1d331050755c37b72
Danny White <3104761+dnywh@users.noreply.github.com>
2024-12-19 21:47:44 +1000

    Static homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,23 @@
 // import SignInButton from "@/components/sign-in-button";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { createClient } from "@/utils/supabase/server";
+// import { createClient } from "@/utils/supabase/server";
 
 export default async function Index() {
-  const supabase = await createClient();
+  // const supabase = await createClient();
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  // const {
+  //   data: { user },
+  // } = await supabase.auth.getUser();
 
-  const { data: profile } = user ? await supabase
-    .from("profiles")
-    .select("first_name")
-    .eq("id", user.id)
-    .single() : { data: null };
+  // const { data: profile } = user ? await supabase
+  //   .from("profiles")
+  //   .select("first_name")
+  //   .eq("id", user.id)
+  //   .single() : { data: null };
 
   return (
     <main className="flex-1 flex flex-col gap-6 px-4">
-      {/* {user ? <Button asChild size="sm" variant={"outline"}><Link href="/profile">{profile?.first_name || user.email}</Link></Button> : <SignInButton />} */}
 
       <h2 className="font-medium text-xl mb-4">Find a home for your food scraps,wherever you are.</h2>
 
@@ -27,11 +26,11 @@ export default async function Index() {
           <Link href="/map">Browse the map</Link>
         </Button>
         <Button asChild size="lg" variant={"outline"}>
-          {user ? <Link href="/profile#TODO-listing-form-for-signed-in-users">Create a listing</Link> : <Link href="/sign-up">Sign up</Link>}
+          {/* TODO: {user ? <Link href="/profile#TODO-listing-form-for-signed-in-users">Create a listing</Link> : <Link href="/sign-up">Sign up</Link>} */}
         </Button>
       </div>
 
-      {!user && <Link href="/profile#TODO-listing-paramters-for-guest-users-to-eventually-come-back-to-via-a-callback-url">Create a map listing</Link>}
+      {/* TODO: {!user && <Link href="/profile#TODO-listing-paramters-for-guest-users-to-eventually-come-back-to-via-a-callback-url">Create a map listing</Link>} */}
 
 
 
