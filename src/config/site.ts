b8b2379609b8cafe2585f6da49e29494c6d35de1
Danny White <3104761+dnywh@users.noreply.github.com>
2025-01-14 19:53:35 +1000

    Handle local Open Graph testing

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,7 @@
 export const siteConfig = {
   name: "Peels",
   description: "Find a home for your food scraps, wherever you are.",
-  url: process.env.NEXT_PUBLIC_APP_URL || "https://www.peels.app",
-  // You can add more site-wide configuration here
+  url: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
   links: {
     about: "/",
     terms: "/terms",
