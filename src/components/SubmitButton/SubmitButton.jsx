fb0850679bcc9b47ebd5abacbd64e3b4a10bdd00
Danny White <3104761+dnywh@users.noreply.github.com>
2025-01-18 18:54:36 +1000

    Fix missing pending state

--- src/components/SubmitButton/SubmitButton.jsx
+++ src/components/SubmitButton/SubmitButton.jsx
@@ -4,9 +4,13 @@ import Button from "@/components/Button";
 
 export default function SubmitButton({
   children,
+  pending = false,
   pendingText = "Loading...",
   ...props
 }) {
+  // TODO: Add a loading state
+  // Look at Next.js server actions and mutations
+  // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations
   return (
     <Button type="submit" aria-disabled={pending} {...props}>
       {pending ? pendingText : children}
