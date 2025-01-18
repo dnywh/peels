cf063b32c3e8c77291f60fbd843fd313e46e99ec
Danny White <3104761+dnywh@users.noreply.github.com>
2025-01-18 16:51:45 +1000

    Revert submit button

--- src/components/SubmitButton/SubmitButton.jsx
+++ src/components/SubmitButton/SubmitButton.jsx
@@ -1,15 +1,12 @@
 "use client";
 
 import Button from "@/components/Button";
-import { useFormStatus } from "react-dom";
 
 export default function SubmitButton({
   children,
   pendingText = "Loading...",
   ...props
 }) {
-  const { pending } = useFormStatus();
-
   return (
     <Button type="submit" aria-disabled={pending} {...props}>
       {pending ? pendingText : children}
